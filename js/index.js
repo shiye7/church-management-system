0599231b424aa3efc350f3f1a8d7cad886deae7e
shiye7 <jcancajas07@gmail.com>
2022-09-02 07:19:34 +0800

    Scroll Transition

--- js/index.js
+++ js/index.js
@@ -1,91 +1,109 @@
 function onLinkClick(e) {
-    const link = $(this);
-    e.preventDefault();
-    const location = link.attr('href');
-    const target = $(location);
-    let top = target?.position()?.top || 0;
-    if (!location.startsWith('#')) {
-        window.location.href = location;
-        return;
-    }
+  const link = $(this);
+  e.preventDefault();
+  const location = link.attr('href');
+  const target = $(location);
+  let top = target?.position()?.top || 0;
+  if (!location.startsWith('#')) {
+    window.location.href = location;
+    return
+  }
 
-    window.scroll({
-        top: top ? top - 130 : 0,
-        left: 0,
-        behavior: "smooth",
-    });
+  window.scroll({
+    top: top ? top - 130 : 0,
+    left: 0,
+    behavior: "smooth",
+  });
 
-
-    
 
 }
 function onLoad() {
-    const body = $(window);
-    const nav = $('.scroll-nav');
-    const mainNav = $('.navbar');
-    body.scroll((val) => {
-        const scrollVal = $(this).scrollTop();
-        const up = $('.scroll-up');
-        console.log(scrollVal);
-        if (scrollVal > 69) {
-            if (up.hasClass('hide')) {
-                up.removeClass('hide');
-            }
-            if (nav.hasClass('hide')) {
-                nav.removeClass('hide');
-            }
-            if (!mainNav.hasClass('invisible')) {
-                mainNav.addClass('invisible');
-            }
+  const body = $(window);
+  const nav = $('.scroll-nav');
+  const mainNav = $('.navbar');
+  const verse = $('.verse');
 
-        } else if (scrollVal <= 69) {
-            if (!up.hasClass('hide')) {
-                up.addClass('hide');
-            }
-            if (!nav.hasClass('hide')) {
-                nav.addClass('hide');
-            }
-            if (mainNav.hasClass('invisible')) {
-                mainNav.removeClass('invisible');
-            }
-        }
-    });
-    $('a').click(onLinkClick);
+  body.scroll((val) => {
+    const scrollVal = $(this).scrollTop();
+    const up = $('.scroll-up');
+    console.log(scrollVal);
+    if (scrollVal > 69) {
+      if (up.hasClass('hide')) {
+        up.removeClass('hide');
+      }
+      if (nav.hasClass('hide')) {
+        nav.removeClass('hide');
+      }
+      if (!mainNav.hasClass('invisible')) {
+        mainNav.addClass('invisible');
+      }
+
+    } else if (scrollVal <= 69) {
+      if (!up.hasClass('hide')) {
+        up.addClass('hide');
+      }
+      if (!nav.hasClass('hide')) {
+        nav.addClass('hide');
+      }
+      if (mainNav.hasClass('invisible')) {
+        mainNav.removeClass('invisible');
+      }
+      if (peace.hasClass('transition')) {
+        peace.removeClass('transition');
+      }
+    };
+
     
-    $('.card-list').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-      });
+    if (scrollVal > 1220) {
+      if (!verse.hasClass('transition')) {
+        verse.addClass('transition');
+      }
+    }
+    else if (scrollVal <= 1220) {
+      if (verse.hasClass('transition')) {
+        verse.removeClass('transition');
+      }
+    }
+  });
+
+
+
+  $('a').click(onLinkClick);
+
+  $('.card-list').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 }
 $(document).ready(onLoad);
